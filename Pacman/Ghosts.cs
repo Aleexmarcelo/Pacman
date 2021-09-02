d15819ae191ad34983c4c5503b52147cb83b1a10
Alex Marcelo <lete_alex@hotmail.com>
2021-09-01 21:34:57 -0300

    Adicionar arquivos de projeto.

---
+++ Pacman/Ghosts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ghosts
+{
+    public class Ghosts
+    {
+        public string cor;
+        public string velocidade;
+        public string dano;
+
+    }
+    public static void Ghost1(String[] args) //Eu não lembro o que colocar nessa linha para não ter erros, ou se é outro código
+    {
+        Ghosts Ghost1 = new Ghosts();
+        Ghost1.cor = "Azul";
+        Ghost1.velocidade = "3.5";
+        Ghost1.dano = "2";
+
+        Ghosts Ghost2 = new Ghosts();
+        Ghost2.cor = "Vermelho";
+        Ghost2.velocidade = "4";
+        Ghost2.dano = "3";
+    }
+
+
+
+}
+}
---
+++ Pacman/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pacman
+{
+    public class Pacman
+    {
+        public int StartXCoordinate = 0;
+        public int StartYCoordinate = 0;
+        private int xStart = 0;
+        private int yStart = 0;
+
+        public Pacman();
+        public void CreatePacman(int StartXCoordinate,int StartYCoordinate)
+        {
+
+            int StartXCoordinate;
+            int StartYCoordinate;
+
+            xStart = StartXCoordinate;
+            yStart = StartYCoordinate;
+        }
+    }
+}
